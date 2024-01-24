48fa8eb954c040781d18054f5f52919894e404a9
ncip48 <mbahcip00@gmail.com>
2024-01-24 12:16:41 +0700

    news user

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Master\News;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+class NewsController extends Controller
+{
+    public function __construct()
+    {
+        $this->menu = "NEWS";
+        $this->url = url('news');
+        $this->title = "News";
+        $this->view = "master.news.";
+    }
+
+    public function index()
+    {
+        $this->authAction('read');
+        $this->authCheckDetailAccess();
+
+        $breadcrumbs = [
+            ['url' => url('/dashboard'), 'title' => 'Home'],
+            ["url" => "#", "title" => "Master"],
+            ['url' => $this->url, 'title' => $this->title],
+        ];
+
+        return view($this->view . 'index')->with('title', $this->title)
+            ->with('url', $this->url)
+            ->with('breadcrumbs', $breadcrumbs)
+            ->with('allowAccess', $this->authAccess());
+    }
+
+    public function list()
+    {
+        $this->authAction('read');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $data = News::with('user')->get();
+
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->make(true);
+    }
+
+    public function show($id)
+    {
+        $this->authAction('read');
+        if ($this->authCheckDetailAccess() !== true) return $this->authCheckDetailAccess();
+
+        $breadcrumbs = [
+            ['url' => url('/dashboard'), 'title' => 'Home'],
+            ["url" => "#", "title" => "Master"],
+            ['url' => $this->url, 'title' => $this->title],
+            ['url' => '#', 'title' => 'Detail'],
+        ];
+
+        $data = News::with('user')->findOrFail($id);
+
+        $data = [
+            "Creator" => $data->user->name ?? "-",
+            "Slug" => $data->slug ?? "-",
+            "Title" => $data->title ?? "-",
+            "Content" => $data->content ?? "-",
+            "Image" => $data->image ?? "-",
+        ];
+
+        return view($this->view . 'detail')->with('title', $this->title)
+            ->with('url', $this->url)
+            ->with('title', 'Detail ' . $this->title)
+            ->with('breadcrumbs', $breadcrumbs)
+            ->with('data', $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,9 @@ Route::group(['middleware' => ['web', 'auth']], function () {
     Route::resource('master/news', App\Http\Controllers\Master\NewsController::class);
     Route::post('master/news/list', [App\Http\Controllers\Master\NewsController::class, 'list'])->name('master.news.list');
     Route::get('master/news/{id}/delete', [App\Http\Controllers\Master\NewsController::class, 'confirm'])->name('master.news.confirm');
+
+    Route::resource('news', App\Http\Controllers\NewsController::class);
+    Route::post('news/list', [App\Http\Controllers\NewsController::class, 'list'])->name('master.news.list');
 });
 
 
